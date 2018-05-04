e4896a4bc5ef7c6c7ca1bad1991a16daf3b9c23e
Jack Murning <jackrichardmurning@gmail.com>
2018-05-04 20:46:46 +0100

    complete project euler 2 ruby

--- project_e2.rb
+++ project_e2.rb
@@ -1,6 +1,16 @@
-x = 1
-
-while x <= 25
-  x += x
-  print x
+f = [1, 1]
+a = 0
+b = 1
+x = 0
+y = 0
+while x <= 4000000
+  x = f[a] + f[b]
+  if x <= 4000000 && x % 2 == 0
+    y += x
+  end
+  f << x
+  a += 1
+  b += 1
 end
+
+puts y 
