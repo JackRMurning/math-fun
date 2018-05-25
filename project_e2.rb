4fdf2b087aab7b36912795d5b29c33da7e2471ce
Jack Murning <jackrichardmurning@gmail.com>
2018-05-25 11:08:37 +0100

    cleaner solution using enumerables

--- project_e2.rb
+++ project_e2.rb
@@ -1,16 +1,4 @@
-f = [1, 1]
-a = 0
-b = 1
-x = 0
-y = 0
-while x <= 4000000
-  x = f[a] + f[b]
-  if x <= 4000000 && x % 2 == 0
-    y += x
-  end
-  f << x
-  a += 1
-  b += 1
-end
-
-puts y 
+fib_sequence = [1, 2]
+fib_sequence << fib_sequence.last(2).sum while fib_sequence.last <= 4_000_000
+fib_sequence.delete_if{ |num| num.odd?}
+p fib_sequence.sum
