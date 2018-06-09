c7cf7f89f51bfa3a449808b6a96f016eeffaf095
Jack Murning <jackrichardmurning@gmail.com>
2018-06-09 19:31:25 +0100

    add prime div fn which finds divisors

--- projects/project_e12.rb
+++ projects/project_e12.rb
@@ -1,3 +1,25 @@
+require "Prime"
+require "pry-byebug"
+
 def triangle_number_gen(number)
   (1..number).reduce(:+)
 end
+
+def prime_divs(number)
+  divisor_tracker = []
+  number_sqrt = Math.sqrt(number).to_i
+  prime_divisors = Prime.first(number_sqrt)
+  while prime_divisors.include?(number) == false
+      prime_divisors.each do |prime|
+        if number % prime == 0
+          divisor_tracker << prime
+          number /= prime
+        end
+      end
+  end
+  divisor_tracker << number
+  return divisor_tracker
+end
+
+# binding.pry
+# nil
--- specs/project_e12_specs.rb
+++ specs/project_e12_specs.rb
@@ -7,4 +7,8 @@ class TestProjectE12 < MiniTest::Test
   def test_triangle__number_gen()
     assert_equal(28, triangle_number_gen(7))
   end
+
+  def test_prime_divs()
+    assert_equal(5, prime_divs(688).length)
+  end
 end
