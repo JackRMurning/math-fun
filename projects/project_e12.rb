65653af3540496d402b3057a1dfd84eb92b57980
Jack Murning <jackrichardmurning@gmail.com>
2018-06-19 14:01:10 +0100

    prime division method add

--- projects/project_e12.rb
+++ projects/project_e12.rb
@@ -1,47 +1,27 @@
 require "Prime"
-require "pry-byebug"
+require "Pry-byebug"
 
-def triangle_number(number)
-  result =  (number*(number + 1)) / 2
+def triangle_number_gen(index_number)
+  result = (index_number * (index_number + 1))/2
   return result
 end
-
-def triangle_list(list_length)
-  result = (1..list_length).map { |n| triangle_number(n)  }
-  return result
-end
-
-
 
 def prime_factors(number)
-  prime_divisors = []
-  while @prime_checklist.include?(number) == false
-      @prime_checklist.each do |prime|
-        if number % prime == 0
-          prime_divisors << prime
-          number /= prime
-        end
-    end
+  result = Prime.prime_division(number)
+  return result
+end
+
+def factor_count(prime_factors_array)
+  total = 1
+  result = prime_factors_array.each do |number|
+    total *= number[1] + 1
   end
-  prime_divisors << number
-  return prime_divisors.sort
+  return total
 end
 
-def prime_list(number)
-  prime_checklist = []
-  Prime.each(number) do |prime|
-    prime_checklist << prime
-  end
-  return prime_checklist
-end
 
-def divisor_group(pfactor_array)
-  divisor_hash = pfactor_array.reduce(Hash.new(0)) { |a, b| a[b] += 1; a }
-  return divisor_hash
-end
 
-def divisor_count(div_hash)
-  count = 1
-  div_hash.each_value {|value| count *= (value+1)}
-  return count
-end
+
+
+binding.pry
+nil
--- specs/project_e12_specs.rb
+++ specs/project_e12_specs.rb
@@ -4,30 +4,10 @@ require_relative('../projects/project_e12.rb')
 
 class TestProjectE12 < MiniTest::Test
 
-  def test_triangle_number()
-    assert_equal(28, triangle_number(7))
+  def test_factor_count()
+    result = factor_count(7)
+    assert_equal(28, result)
   end
 
-  def test_triangle_list()
-    result = [1, 3, 6, 10, 15, 21, 28]
-    assert_equal(result, triangle_list(7))
-  end
-
-  def test_prime_factors()
-    result = [2, 2, 7]
-    @prime_checklist = prime_list(500)
-    assert_equal(result, prime_factors(28))
-  end
-
-  def test_divisor_group()
-    result = divisor_group([2, 2, 7])
-    assert_equal(2, result[2])
-  end
-
-  def test_divisor_count()
-    divHash = divisor_group([2, 2, 7])
-    result = divisor_count(divHash)
-    assert_equal(6, result)
-  end
 
 end
