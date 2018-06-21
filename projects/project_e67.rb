38debcce56070ad1cb521adbe1a30e57b40e7ded
Jack Murning <jackrichardmurning@gmail.com>
2018-06-21 14:15:55 +0100

    complete project 18 & 67 in ruby

--- projects/project_e67.rb
+++ projects/project_e67.rb
@@ -1,7 +1,7 @@
 require "pry-byebug"
 
 triangle = []
-File.open("inputs_for_problems/project_e18.txt").each do |line|
+File.open("inputs_for_problems/project_e67.txt").each do |line|
   number_line = line.chomp.split()
   triangle << number_line.map { |number| number.to_i  }
 end
