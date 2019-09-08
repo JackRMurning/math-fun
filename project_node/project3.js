6467b0363749b2cce82d2aa0d5fa114f3f763d93
JackRMurning <jackrichardmurning@gmail.com>
2019-09-08 18:39:49 +0100

    feat: add project 3 solution

--- project_node/project3.js
+++ project_node/project3.js
@@ -1,3 +1,10 @@
 const primes = require('../functions_node/sieveOfEratothenes');
 
-console.log(primes.sieve(10));
+const largestPrimeFactor = compositeNumber => {
+    const max = Math.floor(Math.sqrt(compositeNumber));
+    const primeList = primes.sieve(max).reverse();
+    const primeFactors = primeList.filter( prime => compositeNumber % prime === 0)
+    return primeFactors[0]
+}
+
+console.log(largestPrimeFactor(600851475143));
