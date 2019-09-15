54f816354691438ca12ec1698579643fc0cefc0a
JackRMurning <jackrichardmurning@gmail.com>
2019-09-16 00:15:58 +0100

    chore: tidy dir and file names

---
+++ functions-node/usefulFunctions.js
@@ -0,0 +1,30 @@
+
+
+const factorize = (number, checkList, factorArray = []) => {
+    
+    if (checkList.includes(number)) { 
+        factorArray.push(number)
+        return factorArray
+    } else if (number < checkList[0]){
+        return factorArray
+    };
+    
+    let factor
+    
+    for (let i of checkList) {
+        if (number % i === 0) {
+            factor = i
+            break
+        }
+    }
+    
+    if (factor){
+    
+        factorArray.push(factor)
+    
+    return factorize(number / factor, checkList, factorArray)
+    }
+    return factorArray
+}
+
+console.log(factorize(33, [2, 2, 3], []))
